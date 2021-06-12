b5b727b782bf4f6d4e9c6cdeb1364e7ff0029d4b
FanJekkk <denteplyakov2020@gmail.com>
2021-06-12 18:46:22 +1000

    Preloader

--- src/components/container.js
+++ src/components/container.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
+
 export default ({ children }) => (
-  <div className = "Firstcontainer" styles = "width: 100%; height: 100%;">{children}</div>
+  <div className = "Firstcontainer" styles = "width: 100%; height: 100%;" >
+  {children}</div>
   
 )
--- src/components/layout.js
+++ src/components/layout.js
@@ -6,22 +6,36 @@
  */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import React from "react"
+import React,{useState, useEffect} from "react"
 import Container from "./container"
 import "./layout.css"
 import Header from './header'
 import NavBar from "./NavBar/NavBar"
+import {FazerAnimation} from 'react-fazer-animation';
+
 
 
 const Layout = ({ children }) => {
+  const [loader, setLoader]=useState(true);
 
+  useEffect(()=>{
+   setTimeout(()=> {
+      setLoader(false)
+   }, 3000)
+  }, [])
   return (
-    <><NavBar />
+    
+    <>{loader ?
+   <div id = "preloader">
+  <FazerAnimation
+    title={'Redirecting'}
+    bodyColor={'#FFFFFF'}
+    longfazersColor={'#FFFFFF'}
+  /></div> :<div><NavBar />
         <Header />
         <Container>
-          
         <main>{children}</main>
-        </Container>
+        </Container></div>}
         
     </>
   )
