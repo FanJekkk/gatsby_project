5cfa08802fa55bbd16826d96dae01d25d414784e
FanJekkk <denteplyakov2020@gmail.com>
2021-11-13 21:00:44 +1000

    test

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,24 +11,10 @@ const About = () => (
     <SEO title="Обо мне" />
     <div className="col-lg-3 col-md-3 col-sm-3"></div>
     <div className="col-lg-3 col-md-3 col-sm-3">
-    <h1>Добро пожаловать на страницу обо мне!</h1>
-    <p>Welcome to About page!</p>
+    <h1>Навыки</h1>
     </div>
     <div className="col-lg-3 col-md-3 col-sm-3">
     <Link to="/">Go back to the homepage</Link>
-    <h1>Hi from the About page</h1>
-    <p>Welcome to page 3</p><h1>Hi from the About page</h1>
-    <p>Welcome to page 3</p><h1>Hi from the About page</h1>
-    <p>Welcome to page 3</p><h1>Hi from the About page</h1>
-    <p>Welcome to page 3</p><h1>Hi from the About page</h1>
-    <p>Welcome to page 3</p><h1>Hi from the About page</h1>
-    <p>Welcome to page 3</p><h1>Hi from the About page</h1>
-    <p>Welcome to page 3</p><h1>Hi from the About page</h1>
-    <p>Welcome to page 3</p><h1>Hi from the About page</h1>
-    <p>Welcome to page 3</p><h1>Hi from the About page</h1>
-    <p>Welcome to page 3</p><h1>Hi from the About page</h1>
-    <p>Welcome to page 3</p><h1>Hi from the About page</h1>
-    <p>Welcome to page 3</p>
     </div>
     <div className="col-lg-3 col-md-3 col-sm-3"></div>
     </div>
